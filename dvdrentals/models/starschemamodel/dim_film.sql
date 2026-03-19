839034173d95f08fdef2d0def3cb847ca9f452f3
Daniel Noce <nocedan@gmail.com>
2026-03-19 12:02:25 -0300

    add starschema.dim_film

---
+++ dvdrentals/models/starschemamodel/dim_film.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with film as (
+  select * from {{ source('dvdrentals','film')}}
+),
+film_category as (
+ select * from {{source('dvdrentals','film_category')}}
+ ),
+joined as (
+ select
+film.*,
+film_category.category_id,
+film_category.last_update as film_category_last_update
+from film join film_category on film.film_id = film_category.film_id
+)
+select 
+  film_id,
+  title,
+  description,
+  release_year,
+  rental_duration,
+  rental_rate,
+  length,
+  replacement_cost,
+  rating,
+  last_update,
+  category_id,
+  film_category_last_update
+from joined
