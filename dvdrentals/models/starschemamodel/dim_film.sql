5f33658d4101c214dcafec968badaa312ab2f695
Daniel Noce <nocedan@gmail.com>
2026-03-20 10:48:42 -0300

    refactor vpc in order to add category table to model

--- dvdrentals/models/starschemamodel/dim_film.sql
+++ dvdrentals/models/starschemamodel/dim_film.sql
@@ -10,12 +10,17 @@ with film as (
 film_category as (
  select * from {{source('dvdrentals','film_category')}}
  ),
+ category as (
+  select * from {{source('dvdrentals','category')}}
+ ),
 joined as (
  select
 film.*,
 film_category.category_id,
-film_category.last_update as film_category_last_update
+film_category.last_update as film_category_last_update,
+category.name as category_name
 from film join film_category on film.film_id = film_category.film_id
+left join category on film_category.category_id = category.category_id
 )
 select 
   {{ dbt_utils.generate_surrogate_key(['film_id']) }} as film_key,
@@ -30,5 +35,6 @@ select
   rating,
   last_update,
   category_id,
-  film_category_last_update
+  film_category_last_update,
+  category_name
 from joined
