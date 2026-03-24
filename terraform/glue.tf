ebde686ba1ed53144585e184e5e6900a0fd0b1d4
Daniel Noce <nocedan@gmail.com>
2026-03-24 16:29:39 -0300

    configure the job to read glue data catalog

--- terraform/glue.tf
+++ terraform/glue.tf
@@ -1,91 +1,139 @@
-# resource "aws_glue_workflow" "example" {
-#   name = "dvdrentals-workflow"
-# }
+resource "aws_glue_workflow" "dvdrentals_workflow" {
+  name = "dvdrentals-workflow"
+}
 
-# resource "aws_glue_trigger" "example-start" {
-#   name          = "trigger-start"
-#   type          = "ON_DEMAND"
-#   workflow_name = aws_glue_workflow.example.name
+resource "aws_glue_trigger" "example-start" {
+  name          = "trigger-start"
+  type          = "ON_DEMAND"
+  workflow_name = aws_glue_workflow.dvdrentals_workflow.name
 
-#   actions {
-#     job_name = "dvdrentals-extraction-tf"
-#   }
-# }
+  actions {
+    job_name = "dvdrentals-extraction-tf"
+  }
+}
 
-# resource "aws_glue_job" "etl_job" {
-#   name              = "dvdrentals-extraction-tf"
-#   description       = "An example Glue ETL job"
-#   role_arn          = aws_iam_role.glue_job_role.arn
-#   glue_version      = "5.0"
-#   max_retries       = 0
-#   timeout           = 2880
-#   number_of_workers = 2
-#   worker_type       = "G.1X"
-#   connections       = [aws_glue_connection.postgres_connection.name]
-#   execution_class   = "STANDARD"
+# IAM role for Glue jobs
+resource "aws_iam_role" "glue_job_role" {
+  name = "glue-job-role"
 
-#   command {
-#     script_location = "s3://${module.s3_bucket_glue_scripts.s3_bucket_id}/jobs/etl_job.py"
-#     name            = "glueetl"
-#     python_version  = "3"
-#   }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
-#   notification_property {
-#     notify_delay_after = 3 # delay in minutes
-#   }
+# Mandatory Glue baseline
+resource "aws_iam_role_policy_attachment" "glue_service" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
 
-#   default_arguments = {
-#     "--job-language"                     = "python"
-#     "--continuous-log-logGroup"          = "/aws-glue/jobs"
-#     "--enable-continuous-cloudwatch-log" = "true"
-#     "--enable-continuous-log-filter"     = "true"
-#     "--enable-metrics"                   = ""
-#     "--enable-auto-scaling"              = "true"
-#   }
+# S3 access
+resource "aws_iam_role_policy_attachment" "glue_s3" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
 
-#   execution_property {
-#     max_concurrent_runs = 1
-#   }
+# Secrets Manager read-only (credentials already created)
+resource "aws_iam_role_policy_attachment" "glue_secrets" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
 
-#   tags = {
-#     "ManagedBy" = "AWS"
-#   }
-# }
+# RDS access
+resource "aws_iam_role_policy_attachment" "glue_rds" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
 
-# # IAM role for Glue jobs
-# resource "aws_iam_role" "glue_job_role" {
-#   name = "glue-job-role"
+# Fetch the secret created automatically by RDS
+data "aws_secretsmanager_secret_version" "db" {
+  secret_id = module.db.db_instance_master_user_secret_arn
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "glue.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+locals {
+  db_credentials = jsondecode(data.aws_secretsmanager_secret_version.db.secret_string)
+}
 
-# resource "aws_s3_object" "glue_etl_script" {
-#   bucket = module.s3_bucket_glue_scripts.s3_bucket_id
-#   key    = "jobs/etl_job.py"
-#   source = "jobs/etl_job.py" # Make sure this file exists locally
-# }
+resource "aws_glue_connection" "postgres_connection" {
+  name = "postgres_connection"
 
-# resource "aws_glue_connection" "postgres_connection" {
-#   name = "postgres_connection"
-#   connection_properties = {
-#     JDBC_CONNECTION_URL = "jdbc:postgresql://${module.db.db_instance_endpoint}/dvdrentals"
-#     PASSWORD            = var.postgres_password
-#     USERNAME            = var.postgres_username
-#   }
-#   physical_connection_requirements {
-#     availability_zone      = module.vpc.azs[0]
-#     security_group_id_list = [aws_security_group.main.id]  # Glue connection security group
-#     subnet_id              = module.vpc.private_subnets[0]
-#   }
-# }
+  connection_properties = {
+    JDBC_CONNECTION_URL = "jdbc:postgresql://${module.db.db_instance_endpoint}/dvdrentals"
+    USERNAME            = local.db_credentials["username"]
+    PASSWORD            = local.db_credentials["password"]
+  }
+
+  physical_connection_requirements {
+    availability_zone      = module.vpc.azs[0]
+    security_group_id_list = [aws_security_group.main.id]
+    subnet_id              = module.vpc.private_subnets[0]
+  }
+}
+
+resource "aws_glue_catalog_database" "dvdrentals_glue_db" {
+  name = "dvdrentals"
+
+  create_table_default_permission {
+    permissions = ["SELECT"]
+
+    principal {
+      data_lake_principal_identifier = "IAM_ALLOWED_PRINCIPALS"
+    }
+  }
+}
+
+resource "aws_glue_job" "etl_job" {
+  name              = "dvdrentals-extraction-tf"
+  description       = "An example Glue ETL job"
+  role_arn          = aws_iam_role.glue_job_role.arn
+  glue_version      = "5.0"
+  max_retries       = 0
+  timeout           = 2880
+  number_of_workers = 2
+  worker_type       = "G.1X"
+  connections       = [aws_glue_connection.postgres_connection.name]
+  execution_class   = "STANDARD"
+
+  command {
+    script_location = "s3://${module.s3_bucket_glue_scripts.s3_bucket_id}/jobs/etl_job.py"
+    name            = "glueetl"
+    python_version  = "3"
+  }
+
+  notification_property {
+    notify_delay_after = 3 # delay in minutes
+  }
+
+  default_arguments = {
+    "--job-language"                     = "python"
+    "--continuous-log-logGroup"          = "/aws-glue/jobs"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
+    "--enable-metrics"                   = ""
+    "--enable-auto-scaling"              = "true"
+    "--enable-glue-datacatalog"          = "true"
+  }
+
+  execution_property {
+    max_concurrent_runs = 1
+  }
+
+  tags = {
+    "ManagedBy" = "AWS"
+  }
+}
+
+resource "aws_s3_object" "glue_etl_script" {
+  bucket = module.s3_bucket_glue_scripts.s3_bucket_id
+  key    = "jobs/etl_job.py"
+  source = "jobs/etl_job.py" # Make sure this file exists locally
+}
